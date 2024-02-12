487f2995eac4ee1d964b8e5a62ae4bafe8c01d7e
ThePeachStar <elle.phuongvong@gmail.com>
2024-02-12 11:24:04 -0800

    updated

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
+        'MONGO_HOST':"mongodb+srv://mpvongie:<RachelPoo1368>@mdr.nvqkgyi.mongodb.net/MDR-BASE?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"MDR-BASE",
         'GOOGLE_CLIENT_ID': "",
         'GOOGLE_CLIENT_SECRET':"",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
